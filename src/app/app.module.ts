f2baeeafc628b08ac7c82205988569df01b96d82
DanielGiraldo-GitHub <daniel.giraldo@ceiba.com.co>
2018-09-14 08:25:39 -0500

    proyecto frod

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ParqueaderoModule } from './app-routing.module';
+import { ParqueaderoComponent } from './parqueadero/parqueadero.component';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ParqueaderoModule
   ],
   providers: [],
   bootstrap: [AppComponent]
